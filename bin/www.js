1c5503d14f0b0de266533d069a385ebbbcd1d2db
남지호 <namjiho@namjihoui-MacBookAir.local>
2024-01-21 22:11:29 +0900

    lalala

--- bin/www.js
+++
@@ -1,9 +0,0 @@
-const PORT = 5001;
-const ip = require("ip");
-const app = require("../app.js");
-
-
-app.listen(
-    PORT,
-    console.log(`server connected at http://${ip.address()}:${PORT}`)
-  );
